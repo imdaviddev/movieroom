9ee90f3ea06c0af5c21dc11383fd030a4f94681e
David Cañete <daviddanielc25@gmail.com>
2025-03-02 20:43:34 -0300

    se agrega el GET de movies

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,20 @@
 #[macro_use] extern crate rocket;
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
-use std::env;
+
+mod routes;
+mod models;
+mod handlers;
 
 #[get("/check_health")]
 fn check_health() -> &'static str {
-    "API is healthy"
-}
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type, Authorization"));
-    }
+    "I'm alive!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    let port = env::var("PORT").unwrap_or_else(|_| "10000".to_string()).parse::<u16>().expect("Invalid port number");
     rocket::build()
-        .attach(CORS)
-        .mount("/", routes![check_health])
-        .configure(rocket::Config {
-            address: "0.0.0.0".parse().unwrap(),
-            port,
-            ..rocket::Config::default()
-        })
+        .mount("/check_health", routes![check_health])
+        .mount("/api", routes![
+            routes::get_movies_route, 
+            routes::get_movie_route
+        ])
 }
---
+++ src/models/movie.rs
@@ -0,0 +1,9 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Movie {
+    pub id: i32,
+    pub title: String,
+    pub director: String,
+    pub year: i32,
+}
